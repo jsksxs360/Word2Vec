d6b76685239e83c76a9143e439878e768c20f0ca
Xusheng <jsksxs360@163.com>
2016-11-18 22:33:42 +0800

    update example code

--- src/me/xiaosheng/word2vec/Test.java
+++ src/me/xiaosheng/word2vec/Test.java
@@ -8,33 +8,52 @@ import java.util.Set;
 
 import com.ansj.vec.domain.WordEntry;
 
+import me.xiaosheng.util.Segment;
+
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 		Word2Vec vec = new Word2Vec();
 		try {
 			vec.loadGoogleModel("data/wiki_chinese_word2vec(Google).model");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		//计算词语相似度
-		System.out.println(vec.wordSimilarity("狗", "猫"));
-		System.out.println(vec.wordSimilarity("计算机", "电脑"));
-		System.out.println(vec.wordSimilarity("计算机", "人"));
-		//计算句子相似度
-		String s1 = "苏州 有 多条 公路 正在 施工 造成 局部 地区 汽车 行驶 非常 缓慢";
-		String s2 = "苏州 最近 有 多条 公路 在 施工 导致 部分 地区 交通 拥堵 汽车 难以 通行";
-		String s3 = "苏州 是 一座 美丽 的 城市 四季 分明 雨量 充沛";
-		System.out.println(vec.sentenceSimilairy(s1, s1));
-		System.out.println(vec.sentenceSimilairy(s1, s2));
-		System.out.println(vec.sentenceSimilairy(s1, s3));
+		System.out.println("-----词语相似度-----");
+		System.out.println("狗|猫: " + vec.wordSimilarity("狗", "猫"));
+		System.out.println("计算机|电脑: " + vec.wordSimilarity("计算机", "电脑"));
+		System.out.println("计算机|人: " + vec.wordSimilarity("计算机", "人"));
 		//获取相似的词语
 		Set<WordEntry> similarWords = vec.getSimilarWords("漂亮", 10);
+		System.out.println("与 [ 漂亮 ] 语义相似的词语:");
 		for(WordEntry word : similarWords) {
 			System.out.println(word.name + " : " + word.score);
 		}
+		//计算句子相似度
+		System.out.println("-----句子相似度-----");
+		String s1 = "苏州有多条公路正在施工，造成局部地区汽车行驶非常缓慢。";
+		String s2 = "苏州最近有多条公路在施工，导致部分地区交通拥堵，汽车难以通行。";
+		String s3 = "苏州是一座美丽的城市，四季分明，雨量充沛。";
+		System.out.println("s1: " + s1);
+		System.out.println("s2: " + s2);
+		System.out.println("s3: " + s3);
+		//分词，去停用词
+		Segment seg1 = new Segment(s1, true);
+		Segment seg2 = new Segment(s2, true);
+		Segment seg3 = new Segment(s3, true);
+		//标准句子相似度
+		System.out.println(vec.sentenceSimilarity(seg1.getWords(), seg1.getWords()));
+		System.out.println(vec.sentenceSimilarity(seg1.getWords(), seg2.getWords()));
+		System.out.println(vec.sentenceSimilarity(seg1.getWords(), seg3.getWords()));
+		//简易句子相似度
+		System.out.println(vec.easySentenceSimilarity(seg1.getWords(), seg1.getWords()));
+		System.out.println(vec.easySentenceSimilarity(seg1.getWords(), seg2.getWords()));
+		System.out.println(vec.easySentenceSimilarity(seg1.getWords(), seg3.getWords()));
+		//简易句子相似度(名词、动词权值设为1，其他设为0.8)
+		System.out.println(vec.easySentenceSimilarity(seg1.getWords(), seg1.getWords(), seg1.getPOSWeightVector(), seg1.getPOSWeightVector()));
+		System.out.println(vec.easySentenceSimilarity(seg1.getWords(), seg2.getWords(), seg1.getPOSWeightVector(), seg2.getPOSWeightVector()));
+		System.out.println(vec.easySentenceSimilarity(seg1.getWords(), seg3.getWords(), seg1.getPOSWeightVector(), seg3.getPOSWeightVector()));
 //		try {
 //			Word2Vec.trainJavaModel("data/train.txt", "data/test.model");
 //		} catch (IOException e) {
@@ -42,5 +61,4 @@ public class Test {
 //			e.printStackTrace();
 //		}
 	}
-
 }
